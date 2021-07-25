582dd27f9e60fed857a3608903783d9bfaa64fdc
John Powers <john.powers070@gmail.com>
2021-07-25 09:26:21 -0400

    initial

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <b-button size="lg">{{ button }}</b-button>
+    <b-button size="lg" class="b-color m-2">{{ buttonName }}</b-button>
   </div>
 </template>
 <script>
 export default {
   name: "Button",
-  props: [{ name: "" }]
+  props: { buttonName: String }
 };
 </script>
+<style scoped>
+.b-color {
+  color: white;
+  font-size: 15px;
+  width: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,48 @@
 <template>
   <div>
-    <!-- needs a grid here-->
+    <b-container>
+      <b-row id="row-1"><b-col cols="12"></b-col></b-row>
+      <b-row id="row-2"><b-col cols="12"></b-col></b-row>
+      <b-row id="row-3">
+        <b-col cols="9"></b-col>
+        <b-col cols="3">
+          <b-row v-for="row in arr" :key="row[1]">
+            <b-col cols="6" v-for="item in row" :key="item"><Button :buttonName="item"/></b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
-<script></script>
+<script>
+import Button from "@/components/Button.vue";
+
+export default {
+  data() {
+    return {
+      arr: [
+        ["one", "two"],
+        ["three", "four"],
+        ["five", "six"],
+        ["seven", "eight"]
+      ]
+    };
+  },
+  components: {
+    Button
+  }
+};
+</script>
+<style scoped>
+#row-1 {
+  height: 30%;
+  width: 100%;
+}
+#row-2 {
+  height: 30%;
+  width: 100%;
+}
+#row-3 {
+  height: 30%;
+}
+</style>
