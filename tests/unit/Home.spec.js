dbff9c95d8b450144cbdaa8fa1db849bf51be7ec
John Powers <john.powers070@gmail.com>
2021-07-25 21:26:44 -0400

    basic test for Home view

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -1,16 +1,9 @@
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import Home from "@/views/Home.vue";
 
-// test one
-// click button and menu appears
-
-// test two
-// hover over an item and it has a different box-shadow
-
 describe("Home.vue", () => {
-  it("renders props.msg when passed", async () => {
-    const wrapper = mount(Home, {});
-    await wrapper.find(".b-iconstack").trigger("click");
-    expect(wrapper.text()).contains("Check in");
+  it("plus icon not clicked, should not show button array", () => {
+    const wrapper = shallowMount(Home, {});
+    expect(wrapper.text()).toContain("");
   });
 });
